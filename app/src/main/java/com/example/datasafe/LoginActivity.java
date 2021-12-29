b7af3d01f3ccb7368d89046e0a31754d3ad8272e
muaaaaz <https://githhub.com/muaaaaz>
2021-12-29 21:36:18 +0500

    Implement login function in LoginActivity.java. Add signup shortcut in LoginActivity.java and activity_login.xml

--- app/src/main/java/com/example/datasafe/LoginActivity.java
+++ app/src/main/java/com/example/datasafe/LoginActivity.java
@@ -1,15 +1,20 @@
 package com.example.datasafe;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.datasafe.dbhelper.DbHelper;
+import com.example.datasafe.models.User;
+
 public class LoginActivity extends AppCompatActivity {
     EditText usernameEditText, passwordEditText;
-    Button loginBtn;
+    Button loginBtn, createAccountBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +24,22 @@ public class LoginActivity extends AppCompatActivity {
         usernameEditText = findViewById(R.id.editText_username);
         passwordEditText = findViewById(R.id.editText_password);
         loginBtn = findViewById(R.id.btn_login);
-        // TODO: 29/12/2021 Implement login on button 
+        createAccountBtn = findViewById(R.id.btn_create_account);
+
+        loginBtn.setOnClickListener(v -> {
+            if (!validateEntries()) return;
+            User user = new User(usernameEditText.getText().toString().trim(), passwordEditText.getText().toString());
+            DbHelper dbHelper = new DbHelper(this);
+            if (dbHelper.verifyUser(user)) {
+                Toast.makeText(this, "Welcome " + user.getUsername() + "!", Toast.LENGTH_SHORT).show();
+            }
+            finish();
+        });
+
+        createAccountBtn.setOnClickListener(v -> {
+            Intent signupIntent = new Intent(this, SignupActivity.class);
+            startActivity(signupIntent);
+        });
     }
 
     @Override
@@ -33,5 +53,28 @@ public class LoginActivity extends AppCompatActivity {
         super.onRestoreInstanceState(savedInstanceState);
         if (savedInstanceState.containsKey("USERNAME"))
             usernameEditText.setText(savedInstanceState.getString("USERNAME"));
+    }
+
+    boolean validateEntries() {
+        String username = usernameEditText.getText().toString().trim().toLowerCase();
+        String password = passwordEditText.getText().toString();
+        boolean isOk = true;
+        if (username.length() < 3) {
+            usernameEditText.setError(getString(R.string.min_length_3));
+            isOk = false;
+        }
+        if (username.length() > 0 && (username.charAt(0) < 'a' || username.charAt(0) > 'z')) {
+            usernameEditText.setError(getString(R.string.first_char_must_be_alpha));
+            isOk = false;
+        }
+        if (username.contains(" ")) {
+            usernameEditText.setError(getString(R.string.no_space_allowed));
+            isOk = false;
+        }
+        if (password.length() < 4) {
+            passwordEditText.setError(getString(R.string.min_length_4));
+            isOk = false;
+        }
+        return isOk;
     }
 }
